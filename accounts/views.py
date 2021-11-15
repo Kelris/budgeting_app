a0a0d7c8aa772c1bb02b8e2fabb870821aa5a579
katarzynazych-sc <katarzynazych@scurri.com>
2021-11-15 21:35:52 +0100

    feature: accounts login

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from accounts.forms import RegisterForm
+from accounts.forms import LoginForm, RegisterForm
 
 
 class RegisterView(CreateView):
     form_class = RegisterForm
-    success_url = reverse_lazy('budget:index')
+    success_url = reverse_lazy('accounts:login')
     template_name = 'accounts/register.html'
+
+
+class Login_View(LoginRequiredMixin, LoginView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
