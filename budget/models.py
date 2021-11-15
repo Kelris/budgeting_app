58f8f7405d49ae6188724e852826b01af1cd8aa6
katarzynazych-sc <katarzynazych@scurri.com>
2021-11-15 16:48:16 +0100

    feature: models amendmends

--- budget/models.py
+++ budget/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
+from datetime import date
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Expenses(models.Model):
+    name = models.CharField(max_length=40, blank=True)
     value = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default='')
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return f"{self.category} {self.value} zł {self.name}"
 
 
 class Income(models.Model):
+    name = models.CharField(max_length=40, blank=True)
     value = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return f"{self.value} zł {self.name}"
